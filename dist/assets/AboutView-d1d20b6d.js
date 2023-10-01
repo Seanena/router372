229d156192074ea904447637f92baab020ecd2bf
Pencil05 <poowadech05@gmail.com>
2023-10-01 23:28:08 +0700

    first commit

---
+++ dist/assets/AboutView-d1d20b6d.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as _}from"./index-5482ec8e.js";const s={},c={class:"about"},n=_("h1",null,"เกี่ยวกับครับ",-1),a=[n];function r(i,d){return t(),o("div",c,a)}const u=e(s,[["render",r]]);export{u as default};
---
+++ src/views/MainPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NewsCard from '../components/NewsCard.vue'
+</script>
+
+<template>
+    <NewsCard></NewsCard>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
